e65bc38d0b3e9a854fa65ee52bd751aaade2ba38
AutomationTest001 <automationtest307@gmail.com>
2023-07-29 12:00:16 +0530

    deleted unwanted files

--- src/test/java/com/itview/testng/SoftAssertTest.java
+++ src/test/java/com/itview/testng/SoftAssertTest.java
@@ -7,7 +7,6 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class SoftAssertTest {
